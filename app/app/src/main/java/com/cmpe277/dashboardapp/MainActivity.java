d44f23cb7274dfeeac73e63d66081594d5517e63
Miyar Karthik Kamath <miyarkarthikkamath@gmail.com>
2024-03-29 15:08:04 -0700

    Added main view changes

--- app/app/src/main/java/com/cmpe277/dashboardapp/MainActivity.java
+++ app/app/src/main/java/com/cmpe277/dashboardapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.cmpe277.dashboardapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.Fragment;
 import android.os.Bundle;
+import android.view.MenuItem;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        // Assuming you want to show the Macroeconomic fragment as the default fragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new MacroeconomicFragment()).commit();
+            bottomNav.setSelectedItemId(R.id.navigation_macroeconomic); // Highlight the Macroeconomic menu item
+        }
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    int itemId = item.getItemId();
+                    if (itemId == R.id.navigation_macroeconomic) {
+                        selectedFragment = new MacroeconomicFragment();
+                    } else if (itemId == R.id.navigation_agriculture) {
+                        selectedFragment = new AgricultureFragment();
+                    } else if (itemId == R.id.navigation_trade) {
+                        selectedFragment = new TradeFragment();
+                    } else if (itemId == R.id.navigation_chatgpt) {
+                        selectedFragment = new ChatGPTFragment(); // Ensure this class is created
+                    }
+
+                    if (selectedFragment != null) {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                                selectedFragment).commit();
+                    }
+
+                    return true; // Return true to display the selected item as the selected menu item
+                }
+            };
+
 }
