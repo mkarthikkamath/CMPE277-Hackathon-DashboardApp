147c3171cd6922bf426f0c537cb6e0f3f7d5b203
Ahmed Zaytoun <ah.zaytoun@gmail.com>
2024-03-30 00:43:34 -0700

    added chat GPT view

--- app/app/src/main/java/com/cmpe277/hackathon_cmpe277/MainActivity.java
+++ app/app/src/main/java/com/cmpe277/hackathon_cmpe277/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
             navController = navHostFragment.getNavController();
         }
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.home,R.id.macro, R.id.agriculture, R.id.trade).build();
+                R.id.home,R.id.macro, R.id.agriculture, R.id.trade, R.id.chatGPTFragment).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
 
